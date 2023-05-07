2cd78d23ea9be4e74ce50655a5fb947b296c80b3
Muhammad Abdullah <m.abdullah0747@gmail.com>
2023-05-07 14:03:29 +0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import { useState, useEffect } from "react";
-// import "./styles.css";
+import { useState } from "react";
 import "./styles2.css";
-import ReactStars from "react-rating-stars-component";
 import Multiselect from "multiselect-react-dropdown";
 
 var data = require("./data.json");
 
-const firstExample = {
-  size: 25,
-  value: 3.5,
-  edit: false,
-  isHalf: true,
-  count: 10,
-};
-
 const ratingOptions = [
-  
-    "1 ★☆☆☆☆☆☆☆☆☆",
-    "2 ★★☆☆☆☆☆☆☆☆",
-    "3 ★★★☆☆☆☆☆☆☆",
-    "4 ★★★★☆☆☆☆☆☆",
-    "5 ★★★★★☆☆☆☆☆",
-    "6 ★★★★★★☆☆☆☆",
-    "7 ★★★★★★★☆☆☆",
-    "8 ★★★★★★★★☆☆",
-    "9 ★★★★★★★★★☆",
-    "10 ★★★★★★★★★★",
-  
-]
+  "1 ★☆☆☆☆☆☆☆☆☆",
+  "2 ★★☆☆☆☆☆☆☆☆",
+  "3 ★★★☆☆☆☆☆☆☆",
+  "4 ★★★★☆☆☆☆☆☆",
+  "5 ★★★★★☆☆☆☆☆",
+  "6 ★★★★★★☆☆☆☆",
+  "7 ★★★★★★★☆☆☆",
+  "8 ★★★★★★★★☆☆",
+  "9 ★★★★★★★★★☆",
+  "10 ★★★★★★★★★★",
+];
 
-const genreOptions = ["Action", "Thriller", "Drama", "Comedy"]
+const genreOptions = ["Action", "Thriller", "Drama", "Comedy"];
 
 export default function App() {
   const [value, setValue] = useState("");
@@ -63,53 +51,70 @@ export default function App() {
     e.length > 0 ? setSelectedGenre(e) : setSelectedGenre([]);
   };
 
+  function getStarRating(rating) {
+    const roundedRating = Math.round(rating * 2) / 2; // round to nearest 0.5
+    const fullStars = Math.floor(roundedRating);
+    const halfStar = roundedRating % 1 !== 0 ? "half" : "";
+    const emptyStars = 5 - fullStars - (halfStar === "half" ? 1 : 0);
+
+    const fullStarStr = fullStars > 0 ? "★".repeat(fullStars) : "";
+    const halfStarStr = halfStar === "half" ? "✬" : "";
+    const emptyStarStr = emptyStars > 0 ? "☆".repeat(emptyStars) : "";
+
+    return `${fullStarStr}${halfStarStr}${emptyStarStr}`;
+  }
+
   return (
     <div className="App">
       <h1>Search Movies</h1>
 
       <div className="search-container">
-        
         <div className="filter">
+          <div className="multiselect-container">
+            <div className="search-inner">
+              <input type="text" value={value} onChange={onSearchChange} />
+              <button onClick={() => onSearch(value)}> Search </button>
+            </div>
+            <div className="dropdownSearch">
+              {data.movies
+                .filter((movie) => {
+                  const searchTerm = value.toLowerCase();
+                  const title = movie.title.toLowerCase();
+                  const cat = movie.category.toLowerCase();
+                  const category = movie.category;
+                  const rating = movie.rating;
 
-        <div className="multiselect-container">
-        <div className="search-inner">
-          <input type="text" value={value} onChange={onSearchChange} />
-          <button onClick={() => onSearch(value)}> Search </button>
-        </div>
-        <div className="dropdownSearch">
-          {data.movies
-            .filter((movie) => {
-              const searchTerm = value.toLowerCase();
-              const title = movie.title.toLowerCase();
-              const category = movie.category;
-              const rating = movie.rating;
-
-              return (
-                searchTerm &&
-                title.includes(searchTerm) &&
-                (selectedGenre.length === 0 ||
-                  selectedGenre.includes(category)) &&
-                (selectedRating.length === 0 ||
-                  (rating >= parseFloat(selectedRating) &&
-                    rating < parseFloat(selectedRating) + 1))
-              );
-            })
-            .slice(0, 10)
-            .map((movie) => (
-              <div
-                onClick={() => onSearch(movie.title)}
-                className="searchDropdown-row"
-                key={movie.movieId}
-              >
-                {movie.title}
-                {movie.rating}
-              </div>
-            ))}
-        </div>
+                  return (
+                    searchTerm &&
+                    (title.includes(searchTerm) || cat.includes(searchTerm)) &&
+                    (selectedGenre.length === 0 ||
+                      selectedGenre.includes(category)) &&
+                    (selectedRating.length === 0 ||
+                      (rating >= parseFloat(selectedRating) &&
+                        rating < parseFloat(selectedRating) + 1))
+                  );
+                })
+                .slice(0, 10)
+                .map((movie) => (
+                  <div
+                    onClick={() => onSearch(movie.title)}
+                    className="searchDropdown-row"
+                    key={movie.movieId}
+                  >
+                    <div className="movie-info">
+                      <span className="movie-title">{movie.title}</span>
+                      <span className="movie-category">{movie.category}</span>
+                    </div>
+                    <span className="movie-rating">
+                      {getStarRating(movie.rating)}
+                    </span>
+                  </div>
+                ))}
+            </div>
           </div>
           <div className="multiselect-container">
             <Multiselect
-            style={{ width: "200px" }}
+              style={{ width: "200px" }}
               isObject={false}
               options={ratingOptions}
               showCheckbox
@@ -137,9 +142,6 @@ export default function App() {
             />
           </div>
         </div>
-
-        {/* <ReactStars {...firstExample} /> */}
-      
       </div>
     </div>
   );
